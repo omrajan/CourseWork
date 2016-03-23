48001b250b55ed2426dedcaf824164719dc70538
0613286 <omrajan@hotmail.com>
2016-03-23 23:42:20 +0000

    Added registration

--- bug.php
+++ bug.php
@@ -153,6 +153,15 @@ include("connection.php");
         </table>
     </fieldset>
 </form>
+
+<body>
+
+<form method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
 </body>
 
 </html>
