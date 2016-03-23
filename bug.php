76d3bf4bf5550ea385dc646e81cffefe33f1e992
0613286 <omrajan@hotmail.com>
2016-03-23 02:08:40 +0000

    initial commit

---
+++ bug.php
@@ -0,0 +1,108 @@
+<?php
+
+include("connection.php");
+include("submit.php");
+
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Bug Entry Form</title>
+    <link rel="stylesheet" type="text/css" href="Styles/Stylesheet.css">
+</head>
+
+<body>
+
+<header>
+    <h1>Please Enter Bug details below!!!</h1>
+
+    <nav>
+        <ul>
+            <li><a href="index.php">Home Page</a></li>
+            <li><a href="bug.php">Bugs</a></li>
+            <li><a href="comment.php">Bugs</a></li>
+            <li><a href="register.php">Register</a></li>
+
+        </ul>
+    </nav>
+</header>
+
+<form method="post" action="">
+    <fieldset>
+        <legend>Bug Entry Form</legend>
+        <table width="400" border="0" cellpadding="10" cellspacing="10">
+            <tr>
+                <td colspan="2" align="center" class="error"><?php echo $msg;?></td>
+            </tr>
+            <tr>
+                <td style="font-weight: bold">
+                    <div align="right"><label for="title">Bug Title</label></div>
+                </td>
+                <td>
+                    <input name="title" type="text" class="input" size="25" required />
+                </td>
+            </tr>
+            <tr>
+                <td style="font-weight: bold">
+                    <div align="right">
+                        <label for="desc">Bug Description</label>
+                    </div>
+                </td>
+                <td>
+                    <input name="desc" type="text" class="input" size="25" required />
+                </td>
+            </tr>
+            <tr>
+                <td height="23" style="font-weight: bold">
+                    <div align="right">
+                        <label for="userID">User ID</label>
+                    </div>
+                </td>
+                <td>
+                    <input name="userID" type="text" class="input" size="25" required />
+                </td>
+            </tr>
+            <tr>
+                <td height="23" style="font-weight: bold">
+                    <div align="right">
+                        <label for="postDate">Date Posted</label>
+                    </div>
+                </td>
+                <td>
+                    <input name="postDate" type="date" class="input" size="25" required />
+                </td>
+            </tr>
+            <tr>
+                <td height="23" style="font-weight: bold">
+                    <div align="right">
+                        <label for="fixDate">Date fixed</label>
+                    </div>
+                </td>
+                <td>
+                    <input name="fixDate" type="date" class="input" size="25" required />
+                </td>
+            </tr>
+            <tr>
+                <td height="23" style="font-weight: bold">
+                    <div align="right">
+                        <label for="fixed">Fixed/infixed</label>
+                    </div>
+                </td>
+                <td>
+                    <input name="fixed" type="text" class="input" size="25" required />
+                </td>
+            </tr>
+            <tr>
+                <td height="23"></td>
+                <td>
+                    <div align="right">
+                        <input type="submit" name="submit" value="Register!" />
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </fieldset>
+</form>
+</body>
+</html>
