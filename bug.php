9829e4e23859325cffd35db78e06a30b9b34fb0b
0613286 <omrajan@hotmail.com>
2016-03-24 21:48:40 +0000

    Added registration

--- bug.php
+++ bug.php
@@ -92,7 +92,7 @@ include("connection.php");
 
 <body>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
+<form method="post" enctype="multipart/form-data">
     Select file to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
