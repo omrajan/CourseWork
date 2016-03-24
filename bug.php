78b300def27ac812a657705adaef009018545cda
0613286 <omrajan@hotmail.com>
2016-03-24 15:54:13 +0000

    Added registration

--- bug.php
+++ bug.php
@@ -90,116 +90,17 @@ include("connection.php");
     </fieldset>
 </form>
 
-<header>
-    <h1>Enter your comments related to Bug!</h1>
-</header>
-
-<form method="post" action="">
-    <fieldset>
-        <table width="400" border="0" cellpadding="10" cellspacing="10">
-            <tr>
-                <td colspan="2" align="center" class="error"><?php echo $msg;?></td>
-            </tr>
-
-            <tr>
-                <td style="font-weight: bold">
-                    <div align="right">
-                        <label for="desc">Comment text</label>
-                    </div>
-                </td>
-                <td>
-                    <input name="desc" type="text" class="input" size="25" required />
-                </td>
-            </tr>
-            <tr>
-                <td height="23" style="font-weight: bold">
-                    <div align="right">
-                        <label for="userID">User Commented</label>
-                    </div>
-                </td>
-                <td>
-                    <input name="userID" type="text" class="input" size="25" required />
-                </td>
-            </tr>
-            <tr>
-                <td height="23" style="font-weight: bold">
-                    <div align="right">
-                        <label for="postDate">Date Posted</label>
-                    </div>
-                </td>
-                <td>
-                    <input name="postDate" type="date" class="input" size="25" required />
-                </td>
-            </tr>
-            <tr>
-                <td height="23" style="font-weight: bold">
-                    <div align="right">
-                        <label for="bugID">Comment belongs to</label>
-                    </div>
-                </td>
-                <td>
-                    <input name="bugID" type="text" class="input" size="25" required />
-                </td>
-            </tr>
-
-            <tr>
-                <td height="23"></td>
-                <td>
-                    <div align="right">
-                        <input type="submit" name="submit" value="Submit!" />
-                    </div>
-                </td>
-            </tr>
-        </table>
-    </fieldset>
-</form>
-
-<body>
 <form method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
+    <table width="350" border="0" cellpadding="1" cellspacing="1" class="box">
+        <tr>
+            <td width="246">
+                <input type="hidden" name="MAX_FILE_SIZE" value="2000000">
+                <input name="userfile" type="file" id="userfile">
+            </td>
+            <td width="80"><input name="upload" type="submit" class="box" id="upload" value=" Upload "></td>
+        </tr>
+    </table>
 </form>
 
-<?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
-?>
-</body>
 
 </html>
