d937c650c4e3a50c2eda3753bbb84f0cacc4c419
0613286 <omrajan@hotmail.com>
2016-03-24 14:33:13 +0000

    Added registration

--- index.php
+++ index.php
@@ -1,18 +1,25 @@
 <?php
 $title = "Home";
 $content = '<img src="Image/bug.jpg" class="imgLeft" />
-<h3>Our Goal</h3>
+<h3>ABOUT THIS PAGE</h3>
 <p>
-    Our Bug Entry System is mainly designed to track all kind of Bugs that has been logged
+    This page is for logging simple Bugs and track that has been logged
     and find methods for preventing programmers from inadvertently introducing bugs
-    while writing software.These include
+    while writing software.
 </p>
 
 <img src="Image/bugtypes.png" class="imgRight"/>
 
-<h3>Title 2</h3>
+<h3>GUIDANCE</h3>
 <p>
-    Write about different kind of software bugs and details a bit ....
+    Only registered users should be able to access the system.
+    If you are a new user, Click on Register here and fill required information.
+
+    Registered users can do followings :
+
+    Post new bugs, bugs are simply a text description of the code error.
+    Attachments: A user should be able to upload an attachment to the bug, this should be a txt file containing some necessary code.
+    Comment on any other bug. Comments are simply text stored to help another user fix a bug. 
 </p>';
 
 include 'Template.php';
