25e2dd38dda04a5cfd72f860190eb24cae625d9f
0613286 <omrajan@hotmail.com>
2016-03-25 06:00:20 +0000

    Added registration

--- Template.php
+++ Template.php
@@ -5,11 +5,9 @@
         <title><?php echo $title; ?></title>
         <link rel="stylesheet" type="text/css" href="Styles/Stylesheet.css" />
     </head>
-    <header>
-        <h1>RP Bug Tracking System</h1>
-    </header>
 
     <body>
+    <h1>RP Bug Tracking System</h1>
         <div id="wrapper">
             <div id="banner">             
             </div>
